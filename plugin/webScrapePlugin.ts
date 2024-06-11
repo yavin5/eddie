3a957fc0a0bcaf462d19a4cf79bce2d91aa408b9
yavin5 <yavinfive464@gmail.com>
2024-06-11 10:30:43 -0300

    Added WebScrapePlugin (wip): LLM functions to access today's web.

---
+++ plugin/webScrapePlugin.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+/**
+ * Implements a web client for scraping data off the web, and
+ * also for posting data to the web.
+ * 
+ * @author Yavin Five
+ */
+class WebScrapePlugin {
+    public constructor() {
+        console.log('WebScrapePlugin loaded.');
+    }
+
+    /**
+     * Uses an HTTP client to perform an HTTP GET request and returns the scraped web response content.
+     * @llmFunction
+     * @param {string} url The HTTP URL to request
+     * @param {string[]} [params] Request parameters as name=value strings.
+     * @returns {string} The HTTP response content
+     */
+    async httpGet(url: string, params?: string[]): Promise<string> {
+        // TODO: Handle HTTP sessions.
+        // TODO: Handle HTTPS?
+        let axiosParams = {};
+        if (params) {
+            for (const param in params) {
+                const parts = param.split('=');
+                if (parts.length == 2) {
+                    const key = parts[0];
+                    const val = parts[1];
+                    Object.assign(axiosParams, { key: val });
+                }
+            }
+        }
+        let response;
+        try {
+            response = await axios.get(url, { params: axiosParams });
+            return response.data;
+        } catch (error) {
+            response = `HTTP GET request error: ${error}`;
+            console.error(response);
+            return response;
+        }
+    }
+}
+
+// The plugin class must be the default export of the plugin file.
+export default WebScrapePlugin;
