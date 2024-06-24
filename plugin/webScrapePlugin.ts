b0fccea7548abcd3450aed64f08aeaf25957f641
yavin5 <yavinfive464@gmail.com>
2024-06-24 05:05:16 -0300

    More relevant web search results.

--- plugin/webScrapePlugin.ts
+++ plugin/webScrapePlugin.ts
@@ -67,10 +67,9 @@ class WebScrapePlugin {
             let textLines: string[] = plainText.split('\n');
             let arrayIndex = 0;
             for (let line of textLines) {
-                console.log("LINE: " + line);
+                //console.log("LINE: " + line);
                 if (!(line.startsWith('url : ') || line.startsWith('title : ') || line.startsWith('description : '))) {
                    textLines[arrayIndex] = '';
-                   console.log("    line removed.");
                 }
                 // Some web sites are too fictional.  We need factual data.
                 line = line.toLowerCase();
@@ -85,7 +84,6 @@ class WebScrapePlugin {
                     for (let index = arrayIndex; index >= 0; index--) {
                         if (textLines[index].startsWith('title : ')) {
                             textLines[index] = '';
-                            console.log("    line removed.");
                             if (textLines[index + 1] && textLines[index + 1].startsWith('description : ')) {
                                 textLines[index + 1] = '';
                             }
