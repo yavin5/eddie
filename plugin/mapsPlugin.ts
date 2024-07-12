790797e6b9b304495fa9389db065c74e41ef9bd2
yavin5 <yavinfive464@gmail.com>
2024-07-12 17:02:10 -0300

    Initial revision of the MapsPlugin.

--- plugin/mapsPlugin.ts
+++ plugin/mapsPlugin.ts
@@ -10,7 +10,7 @@
  * @author Matt Williams
  * @author Yavin Five
  */
-class MapsPlugin {
+export default class MapsPlugin {
     public constructor() {
         console.log('MapsPlugin loaded.');
     }
